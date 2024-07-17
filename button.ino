52031c998f13b3f8006a7a8240f5a6e23ad6d091
Ejin Joby <ejinjoby666@gmail.com>
2024-07-17 15:50:10 +0530

    Add files via upload

---
+++ button.ino
@@ -0,0 +1,17 @@
+const int ledPin =12;
+const int buttonPin =2;
+int buttonState =0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState=digitalRead(buttonPin);
+if (buttonState==HIGH){
+  digitalWrite(ledPin, LOW);
+}else{
+  digitalWrite(ledPin, HIGH);
+}
+}
